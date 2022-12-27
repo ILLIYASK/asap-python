9ee8c99ea77c00aa390feb870d262ce94c3b7f87
ILLIYASK <Illiyask26061999@gmail.com>
2022-12-27 11:55:57 +0530

    single inhritance

---
+++ inheritance.py
@@ -0,0 +1,17 @@
+# inheritance
+# single inhritance
+
+# class parant:
+#     def __init__(self,f,m):
+#         self.father=f
+#         self.mother=m
+# class child(parant):
+#     def __init__(self,f,m,c):
+#         self.child=c
+#         parant.__init__(self,f,m)
+#     def printdet(self):
+#         print(self.father,self.mother,self.child)
+# obj=child("xyz","abc","pqr")
+# obj.printdet()
+
+
