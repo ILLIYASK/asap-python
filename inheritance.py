9ade6b2bd380d58d97a6b50ba66cdf391e0b7683
ILLIYASK <Illiyask26061999@gmail.com>
2022-12-27 13:50:09 +0530

    heirachical inheritance

--- inheritance.py
+++ inheritance.py
@@ -1,17 +1,26 @@
 # inheritance
-# single inhritance
-
-# class parant:
-#     def __init__(self,f,m):
-#         self.father=f
-#         self.mother=m
-# class child(parant):
-#     def __init__(self,f,m,c):
-#         self.child=c
-#         parant.__init__(self,f,m)
-#     def printdet(self):
-#         print(self.father,self.mother,self.child)
-# obj=child("xyz","abc","pqr")
-# obj.printdet()
 
 
+
+# hierarchical inheritance
+#
+class perant:
+    f="father"
+    m="mother"
+    def parent_details(self):
+        print(self.f)
+        print(self.m)
+class child1(perant):
+    c1="abc"
+    def printdet(self):
+        print(self.c1)
+class child2(perant):
+    c2="def"
+    def prinndet(self):
+        print(self.c2)
+obj1=child1()
+obj2=child2()
+obj1.parent_details()
+obj1.printdet()
+obj2.parent_details()
+obj2.prinndet()
